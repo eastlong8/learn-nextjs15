cf2782890a0720688b4cac8af0053d398371f72a
DJ(Dongjin) Min <islandvill09@gmail.com>
2024-12-15 15:41:54 +0900

    parallel request using promise.all

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,3 +1,19 @@
+import { API_URL } from "../../../constants";
+
+async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
 export default async function MovieDetail(
   { params, }:
     Readonly<{
@@ -5,5 +21,8 @@ export default async function MovieDetail(
     }>) {
   const resolvedParams = await Promise.resolve(params);
   const { id } = resolvedParams;
-  return <h1>Movie {id}</h1>
+  console.log("start fetching");
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  console.log("end fetching");
+  return <h1>{movie.title}{videos[0].title}</h1>
 }
